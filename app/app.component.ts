f833352ef7862f8a17a47533e4bb44f9fb2bcaf2
Mark <mark.lishman@gmail.com>
2016-09-17 12:06:20 +0100

    Updated to Angular 2.0 Final release

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,11 @@
 import { Component } from '@angular/core';
 
-import { NgIfComponent } from "./ng-if.component";
-import { NgSwitchComponent } from "./ng-switch.component";
-import { NgForComponent } from "./ng-for.component";
-
 @Component({
     selector: 'app',
     template: `
         <ng-if-template *ngIf="showSection('ng-if-template')"></ng-if-template>
         <ng-switch-template *ngIf="showSection('ng-switch-template')"></ng-switch-template>
         <ng-for-template *ngIf="showSection('ng-for-template')"></ng-for-template>`,
-    directives: [
-        NgIfComponent,
-        NgSwitchComponent,
-        NgForComponent
-    ]
 })
 export class AppComponent {
 
